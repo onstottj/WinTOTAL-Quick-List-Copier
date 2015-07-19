154d725046553c9d376579b32346153d487f2f8e
Jon Onstott <jononstott@gmail.com>
2015-07-18 17:43:16 -0700

    Attempting to load data async (not sure if it works)

--- WinTOTAL Quick List Copier/MainWindow.xaml.cs
+++ WinTOTAL Quick List Copier/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -37,18 +38,20 @@ namespace WinTOTAL_Quick_List_Copier
             UpdateCurrentData(txtDestinationConnStr.Text);
         }
 
-        private void UpdateCurrentData(string connectionString)
+        private async void UpdateCurrentData(string connectionString)
         {
             lbQuicklistUsers.Items.Clear();
+            lbQuicklistUsers.Items.Add("Loading...");
 
             try
             {
                 using (var model = new WintotalModel(connectionString))
                 {
-                    var names = from n in model.QuickListNames
-                                join q in model.QuickLists on n.QLNameID equals q.QLNameID into qlstats
-                                orderby n.Name
-                                select n.Name + " - " + qlstats.Count() + " list(s)";
+                    var names = await (from n in model.QuickListNames
+                                       join q in model.QuickLists on n.QLNameID equals q.QLNameID into qlstats
+                                       orderby n.Name
+                                       select n.Name + " - " + qlstats.Count() + " list(s)").ToListAsync();
+                    lbQuicklistUsers.Items.Clear();
                     foreach (var name in names.Distinct())
                     {
                         lbQuicklistUsers.Items.Add(name);
