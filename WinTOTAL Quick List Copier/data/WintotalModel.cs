cb2f22245280c58dd63214f27f6e8b48a28286ab
Jon Onstott <jononstott@gmail.com>
2015-09-09 19:02:14 -0700

    Quick lists are copied using SQL Server 2005 compatibility

--- WinTOTAL Quick List Copier/data/WintotalModel.cs
+++ WinTOTAL Quick List Copier/data/WintotalModel.cs
@@ -1,7 +1,12 @@
 namespace WinTOTAL_Quick_List_Copier.data
 {
+    using System.Data.Common;
     using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    using System.Data.Entity.Infrastructure.DependencyResolution;
+    using System.Data.SqlClient;
 
+    [DbConfigurationType(typeof(EntityFrameworkDbConfiguration))]
     public partial class WintotalModel : DbContext
     {
         public WintotalModel(string connectionString)
@@ -45,6 +50,46 @@ namespace WinTOTAL_Quick_List_Copier.data
                 .HasMany(e => e.QuickLists)
                 .WithRequired(e => e.QuickListName)
                 .WillCascadeOnDelete(false);
+        }
+    }
+
+
+    /// <summary>
+    /// A configuration class for SQL Server that specifies SQL 2005 compatability.
+    /// </summary>
+    internal sealed class EntityFrameworkDbConfiguration : DbConfiguration
+    {
+        /// <summary>
+        /// The provider manifest token to use for SQL Server.
+        /// </summary>
+        private const string SqlServerManifestToken = @"2005";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EntityFrameworkDbConfiguration"/> class.
+        /// </summary>
+        public EntityFrameworkDbConfiguration()
+        {
+            this.AddDependencyResolver(new SingletonDependencyResolver<IManifestTokenResolver>(new ManifestTokenService()));
+        }
+
+        /// <inheritdoc />
+        private sealed class ManifestTokenService : IManifestTokenResolver
+        {
+            /// <summary>
+            /// The default token resolver.
+            /// </summary>
+            private static readonly IManifestTokenResolver DefaultManifestTokenResolver = new DefaultManifestTokenResolver();
+
+            /// <inheritdoc />
+            public string ResolveManifestToken(DbConnection connection)
+            {
+                if (connection is SqlConnection)
+                {
+                    return SqlServerManifestToken;
+                }
+
+                return DefaultManifestTokenResolver.ResolveManifestToken(connection);
+            }
         }
     }
 }
